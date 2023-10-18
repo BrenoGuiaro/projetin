e219fca7282e5fdb798652209824927e333f2478
Fiote <brenoft14@gmail.com>
2023-10-18 20:39:35 -0300

    projetin

---
+++ sla/treino.js
@@ -0,0 +1,53 @@
+const btn = document.querySelector ('.btn')
+
+btn.addEventListener('click', ()=>{
+    btn.style.backgroundColor = 'green'
+    btn.textContent = 'Sigma🍷'
+})
+
+const voltar = document.querySelector('.voltarB')
+
+voltar.addEventListener('click', ()=>{
+    btn.style.backgroundColor = 'red'
+    btn.textContent = 'Beta'
+})
+
+
+
+
+/*////////////////////////////////////////////////////////*/
+
+const btn2 = document.querySelector ('.btn2')
+
+btn2.addEventListener('click', ()=>{
+    btn2.style.backgroundColor = 'green'
+    btn2.textContent = 'Sigma🍷'
+})    
+
+
+const voltar2 = document.querySelector('.voltarB2')
+
+voltar2.addEventListener('click', ()=>{
+    btn2.style.backgroundColor = 'red'
+    btn2.textContent = 'Beta'
+})
+
+
+
+/*//////////////////////////////////////////////////////////////*/
+
+
+const btn3 = document.querySelector ('.btn3')
+
+btn3.addEventListener('click', ()=>{
+    btn3.style.backgroundColor = 'green'
+    btn3.textContent = 'Sigma🍷'
+})    
+
+
+const voltar3 = document.querySelector('.voltarB3')
+
+voltar3.addEventListener('click', ()=>{
+    btn3.style.backgroundColor = 'red'
+    btn3.textContent = 'Beta'
+})
